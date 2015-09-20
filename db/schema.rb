a54f74a0d8c9cd9311bba1c6283d8b583eceea54
Kensuke Izumi <kensuke1275@gmail.com>
2015-09-20 19:16:19 +0900

    modify

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919024328) do
+ActiveRecord::Schema.define(version: 20150919132047) do
 
   create_table "categolized_images", force: :cascade do |t|
     t.integer  "start_x",           limit: 4
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 20150919024328) do
     t.datetime "updated_at",                  null: false
   end
 
+  create_table "training_images", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "image",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "",    null: false
     t.string   "encrypted_password",     limit: 255, default: "",    null: false
@@ -69,7 +76,6 @@ ActiveRecord::Schema.define(version: 20150919024328) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.integer  "evaluation",             limit: 4,   default: 0
     t.integer  "categolize_count",       limit: 4,   default: 0
     t.datetime "created_at",                                         null: false
     t.datetime "updated_at",                                         null: false
